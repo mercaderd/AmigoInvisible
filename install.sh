72b9c10d183456aec9ac30c678416b86cc6152d7
mercaderd <mercaderd@yahoo.es>
2022-02-24 23:39:34 +0100

    install.sh changes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get update && install default-libmysqlclient-dev
+sudo apt-get update && sudo apt-get install default-libmysqlclient-dev -y
 python3 -m venv venv
 . venv/bin/activate && pip install -r requirements.txt  && python manage.py makemigrations && python manage.py migrate && python manage.py createcachetable && python manage.py createsuperuser && python manage.py collectstatic --noinput
