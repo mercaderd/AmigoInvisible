486704ca561957c041e3da7b61a699351c70fa8a
mercaderd <mercaderd@yahoo.es>
2022-02-24 23:30:51 +0100

    include mysql suport

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+sudo apt-get update && install default-libmysqlclient-dev
 python3 -m venv venv
 . venv/bin/activate && pip install -r requirements.txt  && python manage.py makemigrations && python manage.py migrate && python manage.py createcachetable && python manage.py createsuperuser && python manage.py collectstatic --noinput
