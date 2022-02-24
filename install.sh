52150ab3d21e741fe8a8b40c74222ff6c483af97
mercaderd <mercaderd@yahoo.es>
2022-02-24 23:22:34 +0100

    install-sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 -m venv venv
-. venv/bin/activate && pip install -r requirements.txt  && python manage.py makemigrations && python manage.py migrate && python manage.py createcachetable && python manage.py createsuperuser && python manage.py runscript templates_load && python manage.py collectstatic --noinput
+. venv/bin/activate && pip install -r requirements.txt  && python manage.py makemigrations && python manage.py migrate && python manage.py createcachetable && python manage.py createsuperuser && python manage.py collectstatic --noinput
